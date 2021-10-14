388162f0faf90a1b5d474a7583cb24291650b7a2
Ababssi Sophien <abasop1@gmail.com>
2021-10-14 06:48:41 +0200

    mflqsdfl

--- views/viewCity.php
+++ views/viewCity.php
@@ -1,4 +1,26 @@
-<h3>Toutes les villes du monde :</h3>
+<h3>Quelques Moyennes sur votre selection</h3>
+
+        <?php
+        $nbValeur = count($city);     
+        $totPop=0;
+
+        foreach ($city as $value) {
+        
+              $totPop=$totPop+$value->Population();
+        }
+        ?>
+        <table>
+                <tbody>
+                        <th>
+                                <td><p>Population</p></td>
+                        </th>
+                        <tr>
+                                <td><p>Moyennes</p></td>
+                                <td><p><?= round($totPop/$nbValeur,2)?></p></td>
+                        </tr>
+                </tbody>
+        </table>
+
 <main class="grid">
 <?php
 foreach ($city as $city): ?>
--- views/viewCountry.php
+++ views/viewCountry.php
@@ -1,8 +1,45 @@
-<h3>Tous les pays du monde :</h3>
-<main class="grid">
+<h3>Quelques Moyennes sur votre selection</h3>
+
+        <?php
+        $nbValeur = count($country);     
+        $totSuf=0;
+        $totPop=0;
+        $totLif=0;
+        $totGnp=0;
+        $totGnpOld=0;
+
+        foreach ($country as $value) {
+                
+              $totSuf=$totSuf+$value->Surface();
+              $totPop=$totPop+$value->Population();
+              $totLif=$totLif+$value->LifeExpectancy();
+              $totGnp=$totGnp+$value->GNP();
+              $totGnpOld=$totGnpOld+$value->GNPOld();
+        }
+        ?>
+        <table>
+                <tbody>
+                        <th>
+                                <td><p>Surface</p></td>
+                                <td><p>Population</p></td>
+                                <td><p>Espérance de vie</p></td>
+                                <td><p>GNP</p></td>
+                                <td><p>GNPOld</p></td>
+                        </th>
+                        <tr>
+                                <td><p>Moyennes</p></td>
+                                <td><p><?= round($totSuf/$nbValeur,2)?></p></td>
+                                <td><p><?= round($totPop/$nbValeur,2)?></p></td>
+                                <td><p><?= round($totLif/$nbValeur,2)?></p></td>
+                                <td><p><?= round($totGnp/$nbValeur,2)?></p></td>
+                                <td><p><?= round($totGnpOld/$nbValeur,2)?></p></td>
+                        </tr>
+                </tbody>
+        </table>
+        <main class="grid">
         <?php
         foreach ($country as $country): ?>
-                <div class ="drapCell" style="--background:url(/sources/svg/<?=$country->Code()?>.svg)">
+                <div class ="drapCell" style="--background:url(/sources/svg/<?=$country->Code()?>.svg)">                     
                         <p>NameCountry : <?= $country->NameCountry()?></p>
                         <p>CountryCode : <?= $country->Code() ?></p>                       
                         <p>Continent : <?= $country->Continent()?></p>
@@ -20,4 +57,5 @@
                         <p>Code2 : <?= $country->Code2()?> </p>
                 </div>   
         <?php endforeach; ?>  
+        <p>Nombre de résulat : <?= $nbValeur ?></p>
 </main>
